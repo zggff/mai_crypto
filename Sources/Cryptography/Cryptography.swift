6513473bb93c95b6e859f5c4b8f9c27b405c3715
zggff <gigamaximwachau@gmail.com>
2025-10-01 01:07:17 +0300

    refactoring

--- Sources/Cryptography/Cryptography.swift
+++ Sources/Cryptography/Cryptography.swift
@@ -1,62 +1,15 @@
-enum BitOrder {
+public enum BitOrder {
 	case forward
 	case backward
 }
 
-enum FirstBitIndex {
+public enum FirstBitIndex {
 	case zero
 	case one
 }
 
-extension UInt8 {
-	func bitStr() -> String {
-		var res = "0b"
-		for i in (0...7).reversed() {
-			res += String(self[i] as UInt8)
-		}
-		return res
-	}
-	subscript(i: Int) -> Bool {
-		get {
-			return self & (1 << i) != 0
-		}
-		set(val) {
-			switch val {
-			case true: self |= (1 << i)
-			case false: self &= ~(1 << i)
-			}
-		}
-	}
-	subscript(at: Int) -> UInt8 {
-		get {
-			return (self & (1 << at)) >> at
-		}
-		set(val) {
-			switch val {
-			case 0: self &= ~(1 << at)
-			default: self |= (1 << at)
-			}
-		}
-	}
-}
-
-extension [UInt8] {
-
-	func bit(_ at: Int) -> Bool {
-		return self[at / 8] & (1 << (at % 8)) != 0
-	}
-
-	mutating func setBit(_ at: Int, _ val: Bool) {
-		switch val {
-		case false: self[at / 8] &= ~(1 << (at % 8))
-		case true: self[at / 8] |= (1 << (at % 8))
-		}
-	}
-
-}
-
 // only direct p-block rule is a map where
-func transpose(data: [UInt8], rule: [Int], order: BitOrder, firstBit: FirstBitIndex) -> [UInt8]? {
+public func transpose(data: [UInt8], rule: [Int], order: BitOrder, firstBit: FirstBitIndex) -> [UInt8]? {
 	let shift =
 		switch firstBit {
 		case .one: 1
