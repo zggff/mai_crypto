ae34a1b38fd785e9ef37e9a91b7ea61d61a775f1
zggff <gigamaximwachau@gmail.com>
2025-11-05 10:19:35 +0300

    better search

---
+++ Sources/Cryptography/Task1.swift
@@ -0,0 +1,26 @@
+// only direct p-block rule is a map where
+public func transpose(data: [Byte], rule: [Int], order: BitOrder, firstBit: FirstBitIndex)
+	-> [Byte]?
+{
+	let shift =
+		switch firstBit {
+			case .one: 1
+			default: 0
+		}
+
+	if data.count * 8 % rule.count != 0 {
+		return nil
+	}
+	var b = Array.init(repeating: UInt8(0), count: data.count)
+	for i in 0..<data.count * 8 {
+		let block = i / rule.count
+		let blockShift = rule[i % rule.count] - shift
+		let pos =
+			switch order {
+				case .forward: block * rule.count + blockShift
+				case .backward: (block + 1) * rule.count - blockShift - 1
+			}
+		b.setBit(pos, data.bit(i))
+	}
+	return b
+}
