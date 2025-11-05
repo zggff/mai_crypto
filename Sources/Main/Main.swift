9ca86c78a4ab848099921543065f34ccefa0dc99
zggff <gigamaximwachau@gmail.com>
2025-11-06 00:08:48 +0300

    fix example

--- Sources/Main/Main.swift
+++ Sources/Main/Main.swift
@@ -3,11 +3,11 @@ import Cryptography
 @main
 struct Main {
 	static func main() async throws {
-		let text = Array("123456".utf8)
-		let key = Array("hello".utf8)
+		let text = Array("12345678".utf8)
+		let key = Array("hello,.!".utf8)
         print(key)
-		let encryptor = SymmetricEncryptor(
-			key: key, mode: EncryptionMode.ecb, padding: PaddingMode.zeros, iv: nil, args: [])!
+		let encryptor = try SymmetricEncryptor<DesEncryptor>(
+			key: key, mode: EncryptionMode.ecb, padding: PaddingMode.zeros, iv: nil, args: [])
 		let encrypted = try await encryptor.encrypt(data: text)
         let decrypted = try await encryptor.decrypt(data: encrypted)
         print("encrypted = '\(encrypted)'")
